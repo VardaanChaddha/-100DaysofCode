932e2774205b07c235cdeb8633e8a22713b84fb3
Vardaan Chaddha <vardaanchaddha551@gmail.com>
2025-08-31 23:56:31 +0530

    Q18.c

--- Day009/Q18.c
+++ Day009/Q18.c
@@ -10,18 +10,25 @@ int main()
 
     if (per >= 90 && per <= 100)
         printf("Grade A");
+        
     else if (per >= 80 && per < 90)
         printf("Grade B");
+        
     else if (per >= 70 && per < 80)
         printf("Grade C");
+        
     else if (per >= 60 && per < 70)
         printf("Grade D");
+        
     else if (per >= 50 && per < 60)
         printf("Grade E");
+        
     else if (per < 50 && per >= 0)
         printf("Grade F");
+        
     else
         printf("Invalid input");
 
     return 0;
+
 }

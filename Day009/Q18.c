0db30719d289e9709e8198406deb59e33932a593
Vardaan Chaddha <vardaanchaddha551@gmail.com>
2025-09-01 06:21:06 +0530

    renaming the folders to show correctly in order

--- Day009/Q18.c
+++ Day009/Q18.c
@@ -4,31 +4,35 @@
 
 int main()
 {
-    int per;
-    printf("Enter the value of percentage: ");
-    scanf("%d", &per);
 
-    if (per >= 90 && per <= 100)
-        printf("Grade A");
-        
-    else if (per >= 80 && per < 90)
-        printf("Grade B");
-        
-    else if (per >= 70 && per < 80)
-        printf("Grade C");
-        
-    else if (per >= 60 && per < 70)
-        printf("Grade D");
-        
-    else if (per >= 50 && per < 60)
-        printf("Grade E");
-        
-    else if (per < 50 && per >= 0)
-        printf("Grade F");
-        
-    else
-        printf("Invalid input");
+    int a;
+    printf("Enter marks (in percentage):    ");
+    scanf("%d", &a);
+
+    if (a >= 90 && a <= 100)
+    {
+        printf("Grade A\n Doing great.. keep up the excellent work!\n");
+    }
+
+    if (a >= 80 && a < 90)
+    {
+        printf("Grade B\n Almost there.. just a little extra effort will make you shine!\n");
+    }
+
+    if (a >= 70 && a < 80)
+    {
+        printf("Grade C\n Good but could improve.. focus harder and you'll get even better.\n");
+    }
+
+    if (a >= 60 && a < 70)
+    {
+        printf("Grade D\n Needs improvement.. believe in yourself and take steps to progress.\n");
+    }
+
+    if (a >= 0 && a < 60)
+    {
+        printf("Grade F\n Struggling.. don't give up, keep trying and ask for help when needed.\n");
+    }
 
     return 0;
-
 }

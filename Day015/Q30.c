ff9f338078eb71ed148353e2d291c3978bf242ea
Vardaan Chaddha <vardaanchaddha551@gmail.com>
2025-09-11 00:17:34 +0530

    ..

---
+++ Day015/Q30.c
@@ -0,0 +1,20 @@
+// Q30: Write a program to reverse a given number.
+
+#include <stdio.h>
+
+int main()
+{
+
+    int n, opp = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        opp = n % 10;
+        printf("%d", opp);
+        n = n / 10;
+    }
+
+    return 0;
+}

8d6e77be1dfc0837e7f96cb64c6505e2de6294b8
Vardaan Chaddha <vardaanchaddha551@gmail.com>
2025-12-03 12:40:05 +0530

    Add files via upload

---
+++ Day023/Q46.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//Q46: Write a program to print the following pattern:
+   int main(){ int i = 1;
+               while (i <= 25)
+               {printf("*");
+                if (i % 5 == 0)
+                printf("\n");                                  
+                i += 1;}
+               return 0;
+   }
